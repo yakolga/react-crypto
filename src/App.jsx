53aeb659338354708decd8957e9e93afbc5b1d3b
Olga Kulczycka <olga@webjaksklep.eu>
2025-10-06 20:53:41 +0300

    layout omponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { Layout } from "antd";
+import AppHeader from "./Layout/AppHeader";
+import AppSider from "./Layout/AppSider";
+import AppContent from "./Layout/AppContent";
+
+const footerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#4096ff',
+};
+
+const layoutStyle = {
+  overflow: 'hidden',
+  height: '100vh'
+};
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+  return (
+    <Layout style={layoutStyle}>
+      <AppHeader/>
+      <Layout>
+        <AppSider/>
+        <AppContent/>
+      </Layout>
+    </Layout>
+  )
 }
---
+++ src/Layout/AppContent.jsx
@@ -0,0 +1,15 @@
+import { Layout } from "antd";
+
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#0958d9',
+};
+
+export default function AppContent() {
+    return(
+        <Layout.Content style={contentStyle}>Content</Layout.Content>
+    )
+}
