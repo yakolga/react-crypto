62a6d09c3532c4f9ebcdd739255a1fe2e0954ecb
Olga Kulczycka <olga@webjaksklep.eu>
2025-10-08 22:32:58 +0300

    node js server

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+app.use(express.static('frontend/dist'))
+app.listen(port, () => console.log('Server has been started on port 80... '))
